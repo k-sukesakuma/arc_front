37fd1b75ea4eea4c573ab3ad5db01ebc3124abd1
k-suke39 <pz.mozarla@gmail.com>
2023-09-10 13:05:14 +0900

    ✨ feat: ログイン/ログアウト機能の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import Header from './components/Header';
 import LoginButton from './components/LoginButton';
@@ -6,9 +8,31 @@ import Footer from './components/Footer';
 
 import Motion from './components/motionWrapper/MotionWrapper';
 
-export default async function Home() {
+import { useSession } from 'next-auth/react';
+
+import Login from './components/Login';
+import Logout from './components/Logout';
+
+export default function Home() {
+	const { data: session, status } = useSession();
 	return (
 		<div>
+			{status === 'authenticated' ? (
+				<div>
+					<p>セッションの期限：{session.expires}</p>
+					<p>ようこそ、{session.user?.name}さん</p>
+					<img
+						src={session.user?.image ?? ``}
+						alt=""
+						style={{ borderRadius: '50px' }}
+					/>
+					<div>
+						<Logout />
+					</div>
+				</div>
+			) : (
+				<Login />
+			)}
 			<Motion>
 				<main className="relative">
 					<section className="relative mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 overflow-hidden">
