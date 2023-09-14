9f4f5aa8a5486e964395e29f27dc0a03c927481c
k-suke39 <pz.mozarla@gmail.com>
2023-09-14 23:43:39 +0900

    🔥 mod: トップページのボタンのデザイン変更

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer className="px-4 sm:px-6 lg:px-8">
-			<div className="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
-				<div className="flex items-center justify-center space-x-5">
+			<div className="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-10">
+				<div className="flex items-center justify-center space-x-5 my-5">
 					<p className="mt-2 font-bold text-base leading-7 text-slate-600 ">
 						利用規約
 					</p>
