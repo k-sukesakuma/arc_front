2c206fcb03d607fa2ecd0a566b651f373379152c
k-suke39 <pz.mozarla@gmail.com>
2023-09-14 13:52:43 +0900

    🔥 fix: Codemirrorからmonacoに変更により表示速度の修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,17 @@ import LoginButton from './components/LoginButton';
 import TimeLine from './components/TimeLine';
 import Footer from './components/Footer';
 
-import Motion from './components/motionWrapper/MotionWrapper';
-
 export default async function Home() {
 	return (
 		<div>
-			<Motion>
-				<main className="relative">
-					<section className="relative mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 overflow-hidden">
-						<Header />
-					</section>
-				</main>
-				<LoginButton />
-				<TimeLine />
-				<Footer />
-			</Motion>
+			<main className="relative">
+				<section className="relative mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 overflow-hidden">
+					<Header />
+				</section>
+			</main>
+			<LoginButton />
+			<TimeLine />
+			<Footer />
 		</div>
 	);
 }
