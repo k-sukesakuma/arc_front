19660d2b56095c6a670de6f608973b55429c66ac
k-suke39 <pz.mozarla@gmail.com>
2023-09-17 10:32:26 +0900

    ✨ feat:ログイン機能の実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import NextAuthProvider from '@/providers/NextAuth';
+import ToastProvider from '@/providers/toast.provider';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata: Metadata = {
 	title: 'ActiveRecord学習サービス | current_user',
@@ -15,7 +17,9 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<NextAuthProvider>{children}</NextAuthProvider>
+				<ToastProvider>
+					<NextAuthProvider>{children}</NextAuthProvider>
+				</ToastProvider>
 			</body>
 		</html>
 	);
