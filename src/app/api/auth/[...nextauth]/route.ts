9fb1ee12263edc8266a86b8babe7a6b8d4c74c56
k-suke39 <pz.mozarla@gmail.com>
2023-09-18 18:07:59 +0900

    add: 環境変数の命名変更

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ import axios from 'axios';
 const handler = NextAuth({
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID || '',
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
+			clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || '',
 		}),
 	],
 	secret: process.env.NEXTAUTH_SECRET || '',
