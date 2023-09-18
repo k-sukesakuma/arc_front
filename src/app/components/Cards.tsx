b47f37b791e2405429f64435e3af3d92881ee6ec
k-suke39 <pz.mozarla@gmail.com>
2023-09-19 05:36:59 +0900

    🐛 mod: 上級編の非表示

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 export default function Cards() {
 	return (
 		<div>
-			{' '}
 			<div className="flex justify-around">
 				<div className="flex-1 m-4 p-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 					<Link href="/works/trial">
@@ -13,7 +12,7 @@ export default function Cards() {
 						</h5>
 					</Link>
 					<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-						ActiveRecorderの操作に慣れてみよう！
+						操作に慣れてみよう！
 					</p>
 					<Link
 						href="/works/trial"
@@ -44,7 +43,7 @@ export default function Cards() {
 						</h5>
 					</Link>
 					<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-						ActiveRecorderの操作に慣れてみよう！
+						current_userを使ってデータを取得してみよう!
 					</p>
 					<Link
 						href="/works/basic"
@@ -77,7 +76,7 @@ export default function Cards() {
 						</h5>
 					</Link>
 					<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-						ActiveRecorderの操作に慣れてみよう！
+						複雑な条件でデータを取得してみよう!
 					</p>
 					<Link
 						href="/works/intermediate"
@@ -101,36 +100,11 @@ export default function Cards() {
 						</svg>
 					</Link>
 				</div>
-				<div className="flex-1 m-4 p-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-					<Link href="/works/advanced">
-						<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-							上級編
-						</h5>
-					</Link>
-					<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-						ActiveRecorderの操作に慣れてみよう！
-					</p>
-					<Link
-						href="/works/advanced"
-						className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-800 rounded-lg hover:bg-gray-700"
-					>
-						こちらから
-						<svg
-							className="w-3.5 h-3.5 ml-2"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 14 10"
-						>
-							<path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M1 5h12m0 0L9 1m4 4L9 9"
-							/>
-						</svg>
-					</Link>
+				<div className="flex-1 m-4 p-10 bg-gray-500 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+					<h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+						上級編
+					</h5>
+					<p className="mb-3 font-normal text-white">ただいま準備中です.....</p>
 				</div>
 			</div>
 		</div>
