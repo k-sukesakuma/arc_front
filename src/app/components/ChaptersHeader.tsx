23d014ba10630bfc37ac4d4fdfb9fb786e57615a
k-suke39 <pz.mozarla@gmail.com>
2023-09-19 08:15:38 +0900

    add

--- src/app/components/ChaptersHeader.tsx
+++ src/app/components/ChaptersHeader.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import useSWR from 'swr';
 import axios from 'axios';
 
 export default function WorksHeader() {
 	const pathname = usePathname();
 	const slug = pathname.substring(7);
-	const [data, setData] = useState<Data | null>(null);
 
 	const getChapterName = (slug: String) => {
 		switch (slug) {
@@ -22,26 +22,15 @@ export default function WorksHeader() {
 	};
 	const displayChapterName = getChapterName(slug);
 
+	const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+	const { data, error } = useSWR(
+		`http://localhost:3001/api/v1/chapters?slug=${slug}`,
+		fetcher
+	);
+
 	useEffect(() => {
-		axios
-			.get(`http://localhost:3001/api/v1/chapters?slug=${slug}`)
-			.then((res) => {
-				setData(res.data);
-				console.log(res.data);
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	}, [slug]);
-
-	type Chapter = {
-		name: string;
-	};
-
-	type Data = {
-		chapters: Chapter[];
-		description: string;
-	};
+		console.log(data);
+	}, [data]);
 
 	return (
 		<header className="px-4 sm:px-6 lg:px-8">
@@ -55,21 +44,18 @@ export default function WorksHeader() {
 			<div>
 				<div className="shadow-md px-6 py-4 my-10 mx-20 rounded-lg bg-white">
 					<div className="text-xl font-bold text-gray-900 px-3 mb-2">
-						【全{data && data.chapters.length}問】 {data && data.description}
+						【全{data.chapters.length}問】 {data.description}
 					</div>
 					<hr className="mt-1 mb-2 rounded-lg" />
 					<div>
-						{data &&
-							data.chapters.map((chapter: any, index: any) => (
-								<div
-									key={index}
-									className="px-4 py-3 font-semibold rounded-lg hover:bg-slate-200"
-								>
-									<Link href={`/works/${slug}/${index + 1}`}>
-										{chapter.name}
-									</Link>
-								</div>
-							))}
+						{data.chapters.map((chapter: any, index: any) => (
+							<div
+								key={index}
+								className="px-4 py-3 font-semibold rounded-lg hover:bg-slate-200"
+							>
+								<Link href={`/works/${slug}/${index + 1}`}>{chapter.name}</Link>
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
