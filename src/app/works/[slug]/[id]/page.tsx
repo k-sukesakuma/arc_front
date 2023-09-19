e5f0f95d8ef04185642d985a376c2f2acbec95f5
k-suke39 <pz.mozarla@gmail.com>
2023-09-19 16:47:33 +0900

    ✨

--- src/app/works/[slug]/[id]/page.tsx
+++ src/app/works/[slug]/[id]/page.tsx
@@ -38,11 +38,6 @@ import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 
 import TwitterIcon from '@mui/icons-material/Twitter';
 
-import { useSWRConfig } from 'swr';
-
-const apiUrl = process.env.NEXTAUTH_URL_INTERNAL;
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
 interface Column {
 	id: 'name' | 'code' | 'population' | 'size' | 'density';
 	label: string;
@@ -94,24 +89,6 @@ function createData(
 	const density = population / size;
 	return { name, code, population, size, density };
 }
-
-const rows = [
-	createData('India', 'IN', 1324171354, 3287263),
-	createData('China', 'CN', 1403500365, 9596961),
-	createData('Italy', 'IT', 60483973, 301340),
-	createData('United States', 'US', 327167434, 9833520),
-	createData('Canada', 'CA', 37602103, 9984670),
-	createData('Australia', 'AU', 25475400, 7692024),
-	createData('Germany', 'DE', 83019200, 357578),
-	createData('Ireland', 'IE', 4857000, 70273),
-	createData('Mexico', 'MX', 126577691, 1972550),
-	createData('Japan', 'JP', 126317000, 377973),
-	createData('France', 'FR', 67022000, 640679),
-	createData('United Kingdom', 'GB', 67545757, 242495),
-	createData('Russia', 'RU', 146793744, 17098246),
-	createData('Nigeria', 'NG', 200962417, 923768),
-	createData('Brazil', 'BR', 210147125, 8515767),
-];
 
 const style = {
 	position: 'absolute' as 'absolute',
@@ -191,7 +168,6 @@ const Page = () => {
 	const id = params.id;
 
 	const getChapterName = (slug: string | string[]) => {
-		// slugが文字列配列の場合、最初の要素を使用
 		const slugStr = Array.isArray(slug) ? slug[0] : slug;
 		switch (slugStr) {
 			case 'trial':
@@ -242,41 +218,6 @@ const Page = () => {
 			: null,
 		fetcher
 	);
-
-	useEffect(() => {
-		if (executionsError) {
-			console.log(executionsError);
-			console.log('エラー');
-		}
-		if (answersData) {
-			console.log(answersData);
-			console.log('データが取得できた');
-			setModalContent(answersData.result);
-		}
-	}, [answersData, executionsError]);
-
-	useEffect(() => {
-		if (executionsError) {
-			console.log('エラー');
-		}
-		if (executionsData) {
-			console.log(executionsData);
-			console.log('データが取得できた');
-		}
-	}, [executionsData, executionsError]);
-
-	useEffect(() => {
-		if (answerPracticesError) {
-			console.log(answerPracticesError);
-			console.log('エラー');
-		}
-		if (answerPracticesData) {
-			console.log('問題');
-			console.log('answerPracticesData');
-			console.log(answerPracticesData);
-			console.log('データが取得できた');
-		}
-	}, [answerPracticesData, answerPracticesError]);
 
 	// ------------------------------------------------------------------------
 
@@ -407,7 +348,6 @@ const Page = () => {
 											}}
 										/>
 									</TabPanel>
-
 									<TabPanel value="3">
 										{executionsData && 'result' in executionsData ? (
 											<div className="height">{executionsData.result}</div>
