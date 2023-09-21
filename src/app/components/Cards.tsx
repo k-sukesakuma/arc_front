ec2eb39d2b34dc46f2de899dd650adbea1505b90
k-suke39 <pz.mozarla@gmail.com>
2023-09-21 12:04:10 +0900

    🐛 : 、半角へ変更

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -12,7 +12,7 @@ export default function Cards() {
 						</h5>
 					</Link>
 					<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-						操作に慣れてみよう！
+						操作に慣れてみよう!
 					</p>
 					<Link
 						href="/works/trial"
@@ -100,7 +100,7 @@ export default function Cards() {
 						</svg>
 					</Link>
 				</div>
-				<div className="flex-1 m-4 p-10 bg-gray-500 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+				<div className="flex-1 m-4 p-10 bg-gray-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 					<h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
 						上級編
 					</h5>
