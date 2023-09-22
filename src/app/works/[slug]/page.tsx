c66855bff7fc74008ea5719f041de10df867dad0
k-suke39 <pz.mozarla@gmail.com>
2023-09-22 11:08:26 +0900

    ⚡️ perf: モーションの削除

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -2,16 +2,13 @@
 import React from 'react';
 import Aside from '@/app/components/Aside';
 import Card from '@/app/components/chapters/Card';
-import Motion from '@/app/components/motionWrapper/motionWrapper';
 const page = () => {
 	return (
 		<div>
 			<main className="flex bg-slate-100 relative">
 				<Aside />
 				<section className="flex-grow">
-					<Motion>
-						<Card />
-					</Motion>
+					<Card />
 				</section>
 			</main>
 		</div>
