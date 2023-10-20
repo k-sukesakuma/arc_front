a2f227e208c386b5b593ebc385e9664f3009e32f
k-suke39 <pz.mozarla@gmail.com>
2023-10-20 13:07:44 +0900

    ⚡️ add: 上級編の追加

--- src/app/components/works/Card.tsx
+++ src/app/components/works/Card.tsx
@@ -111,13 +111,36 @@ export default function Cards() {
 							</svg>
 						</Link>
 					</div>
-					<div className="flex-1 m-4 p-10 bg-gray-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-						<h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-							上級編
-						</h5>
-						<p className="mb-3 font-normal text-white">
-							ただいま準備中です.....
+					<div className="flex-1 m-4 p-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+						<Link href="/works/advanced">
+							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+								上級編
+							</h5>
+						</Link>
+						<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+							複数テーブルの複雑な条件でデータを取得してみよう!
 						</p>
+						<Link
+							href="/works/advanced"
+							className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-slate-800 rounded-lg hover:bg-slate-500"
+						>
+							こちらから
+							<svg
+								className="w-3.5 h-3.5 ml-2"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 10"
+							>
+								<path
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M1 5h12m0 0L9 1m4 4L9 9"
+								/>
+							</svg>
+						</Link>
 					</div>
 				</div>
 			</Motion>
