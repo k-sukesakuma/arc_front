b6b4451e914e0b02ed00bb35c1b92d8c44186919
k-suke39 <pz.mozarla@gmail.com>
2023-10-20 21:46:56 +0900

    ⚡️improve: 高さ変更

--- src/app/works/[slug]/[id]/page.tsx
+++ src/app/works/[slug]/[id]/page.tsx
@@ -272,7 +272,7 @@ const Page = () => {
 											</div>
 										</TabList>
 									</Box>
-									<TabPanel value="1">
+									<TabPanel value="1" sx={{ height: 478 }}>
 										<CodeEditor onMount={handleEditorDidMount} />
 									</TabPanel>
 								</TabContext>
