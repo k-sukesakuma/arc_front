af1d52ed14f1a20e88a6a0c45213d0818d7d0c27
k-suke39 <pz.mozarla@gmail.com>
2023-10-21 10:32:09 +0900

    ⚡️ improve: ローディングの表示

--- src/app/components/chapters/Card.tsx
+++ src/app/components/chapters/Card.tsx
@@ -6,6 +6,7 @@ import useSWR from 'swr';
 import axios from 'axios';
 
 import { useParams } from 'next/navigation';
+import { CircularProgress } from '@mui/material';
 
 export default function WorksHeader() {
 	const fetcher = (url: string) => axios.get(url).then((res) => res.data);
@@ -32,48 +33,61 @@ export default function WorksHeader() {
 		description: string;
 	} | null>(null);
 	const [error, setError] = useState(null);
-
+	const [loading, setLoading] = useState(false);
 	useEffect(() => {
+		setLoading(true);
 		axios
 			.get(
 				`https://current-user-back.onrender.com/api/v1/chapters?slug=${slug}`
 			)
 			.then((res) => {
 				setData(res.data);
+				setLoading(false);
 			})
 			.catch((err) => {
 				setError(err);
+				setLoading(false);
 			});
 	}, [slug]);
 
 	return (
-		<div className="px-4 sm:px-6 lg:px-8">
-			<div className="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
-				<div className="flex items-center justify-center">
-					<h1 className="text-3xl font-semibold tracking-tight text-slate-700">
-						{displayChapterName}
-					</h1>
+		<div>
+			{loading ? (
+				<div className="flex items-center justify-center h-screen  w-screen">
+					<CircularProgress style={{ color: 'grey' }} />
 				</div>
-			</div>
-			<div>
-				<div className="shadow-md px-6 py-4 mx-20 rounded-lg bg-white">
-					<div className="text-xl font-bold text-gray-900 px-3 mb-2">
-						【全{data?.chapters?.length}問】 {data?.description}
+			) : (
+				<div className="px-4 sm:px-6 lg:px-8">
+					<div className="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
+						<div className="flex items-center justify-center">
+							<h1 className="text-3xl font-semibold tracking-tight text-slate-700">
+								{displayChapterName}
+							</h1>
+						</div>
 					</div>
-
-					<hr className="mt-1 mb-2 rounded-lg" />
 					<div>
-						{data?.chapters?.map((chapter: any, index: any) => (
-							<div
-								key={index}
-								className="px-4 py-3 font-semibold rounded-lg hover:bg-slate-100"
-							>
-								<Link href={`/works/${slug}/${index + 1}`}>{chapter.name}</Link>
+						<div className="shadow-md px-6 py-4 mx-20 rounded-lg bg-white">
+							<div className="text-xl font-bold text-gray-900 px-3 mb-2">
+								【全{data?.chapters?.length}問】 {data?.description}
 							</div>
-						))}
+
+							<hr className="mt-1 mb-2 rounded-lg" />
+							<div>
+								{data?.chapters?.map((chapter: any, index: number) => (
+									<div
+										key={index}
+										className="px-4 py-3 font-semibold rounded-lg hover:bg-slate-100"
+									>
+										<Link href={`/works/${slug}/${index + 1}`}>
+											{chapter.name}
+										</Link>
+									</div>
+								))}
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
