36419bdbca8ab7ca3310474fe551a2138c096194
k-suke39 <pz.mozarla@gmail.com>
2023-12-30 16:50:36 +0900

    [add]深夜時間帯にロゴ変更機能の追加

--- src/app/components/aside/Logo.tsx
+++ src/app/components/aside/Logo.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
 export default function Logo() {
+	const [logoProps, setLogoProps] = useState({
+		src: '/logo.png',
+		width: 40,
+		height: 40,
+	});
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			const hour = new Date().getHours();
+			if (hour >= 26 && hour < 27) {
+				setLogoProps({ src: '/nemure_current.png', width: 60, height: 60 });
+			} else {
+				setLogoProps({ src: '/logo.png', width: 40, height: 40 });
+			}
+		}, 1000);
+		return () => clearInterval(timer);
+	}, []);
 	return (
 		<div>
 			<Link href="/">
-				<Image src="/logo.png" alt="Logspot" width={40} height={40} />
+				<Image
+					src={logoProps.src}
+					alt="Logspot"
+					width={logoProps.width}
+					height={logoProps.height}
+				/>
 			</Link>
 		</div>
 	);
